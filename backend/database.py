db5690c2414a51543b6855dc6d699178d470901b
ANSHAM MAURYA <143842290+ANSHAM1@users.noreply.github.com>
2024-07-07 18:18:26 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,4 @@
-from backend.class_defination import Organizer
+from class_defination import Organizer # type: ignore
 
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
