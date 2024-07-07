14a6f1190a71f1a41611c680a542a184b9367ee4
ANSHAM MAURYA <143842290+ANSHAM1@users.noreply.github.com>
2024-07-07 18:15:35 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+#if using vertual environment attach your python.exe path to the frontend and backend veriable
 import subprocess
 import time
 
@@ -7,3 +8,13 @@ backend = subprocess.Popen(['Scripts/python.exe', 'backend/flask_server.py'])
 time.sleep(15)
 
 run_application = subprocess.Popen(['Scripts/python.exe', 'GPS/GPS.py'])
+
+
+#if not using the vertual environment----
+#dont run the app.py
+#run the three py script seperately in different terminals--frontend,backend,GPS
+#important
+#run these script from base folder only to avoid path errors
+#python frontend/frontend.py
+#python backend/flask_server.py
+#python GPS/GPS.py
